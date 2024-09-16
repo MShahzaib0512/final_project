cbcdc769dab3d27c2a0fd022d15230eafa1bb919
MShahzaib0512 <146122274+MShahzaib0512@users.noreply.github.com>
2024-09-16 12:03:29 +0500

    convert to static

--- ecommerce/urls.py
+++ ecommerce/urls.py
@@ -2,5 +2,8 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
- path('',views.index, name='index')
+ path('',views.index, name='index'),
+ path('product',views.product, name='product'),
+ path('about',views.about, name='about'),
+ path('faq',views.faq, name='faq'),
 ]
--- ecommerce/views.py
+++ ecommerce/views.py
@@ -3,3 +3,12 @@ from django.shortcuts import render
 # Create your views here.
 def index(request):
  return render(request,'index.html')
+
+def product(request):
+ return render(request,'product.html')
+
+def about(request):
+ return render(request,'about_us.html')
+
+def faq(request):
+ return render(request ,'faq.html')
